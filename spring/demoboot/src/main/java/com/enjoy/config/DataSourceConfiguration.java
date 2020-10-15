ce5eb7bbb7acd40f8ac697ed50be8221f4003e11
micwsx <812857883@qq.com>
2020-10-15 10:57:13 +0800

    add rmi, rpc, udp sample.

--- spring/demoboot/src/main/java/com/enjoy/config/DataSourceConfiguration.java
+++ spring/demoboot/src/main/java/com/enjoy/config/DataSourceConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
